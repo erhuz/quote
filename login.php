3a37cdcf764e36ad51ce24cd87a79cfaa45631f9
Benjamin Fransson <benjamin@cafcon.se>
2018-04-13 15:26:20 +0200

    Added head template

--- login.php
+++ login.php
@@ -2,17 +2,10 @@
     
     if(isset($_POST['username']))
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css">
-    <title>Login | quotinator</title>
-</head>
-<body>
+
     <?
+        require("templates/head.php");
+        require("templates/header.php");
         require("templates/nav.php");
     ?>
 
