253b651e7834476b1b102f202c3a4b0b07e81492
Benjamin Fransson <benjamin@cafcon.se>
2018-04-13 16:27:18 +0200

    Added basic code

---
+++ profile.php
@@ -0,0 +1,26 @@
+<?php
+    
+    if(isset($_POST['username'])){
+        session_start();
+        
+    }
+?>
+
+<?php
+    require("templates/head.php");
+    require("templates/header.php");
+    require("templates/nav.php");
+?>
+
+    <main class="container">
+        <div class="row">
+            
+        
+        </div>
+    </main>
+    <?php
+        require("templates/footer.php");
+        require("templates/scripts.php");
+    ?>
+</body>
+</html>
