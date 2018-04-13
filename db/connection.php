b6f071eb1d7358c53e2eab71d755b779aed0773c
Benjamin Fransson <benjamin@cafcon.se>
2018-04-13 18:15:20 +0200

    Added credentials

--- db/connection.php
+++ db/connection.php
@@ -1,2 +1,8 @@
 <?php
 // This file is solidarily for setting up the connection variables
+
+define("db_servername", "localhost");
+define("db_port", "3306");
+define("db_dbname", "quote");
+define("db_username", "dbaccess");
+define("db_password", "Xh4k7x");
