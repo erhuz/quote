67e243c53a578dd24b5c6f30ec2ba15e0d08b8a5
Benjamin Fransson <benjamin@cafcon.se>
2018-04-13 17:12:27 +0200

    Changed to a universal title

--- templates/head.php
+++ templates/head.php
@@ -6,6 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css">
     <link rel="stylesheet" href="css/main.min.css">
-    <title>Login | quotinator</title>
+    <title>Quotinator</title>
 </head>
 <body>
