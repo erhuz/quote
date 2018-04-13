464d950d969e0966789372a3c8d6765171755a15
Benjamin Fransson <benjamin@cafcon.se>
2018-04-13 18:15:36 +0200

    Added brand link

--- templates/nav.php
+++ templates/nav.php
@@ -1,6 +1,6 @@
     
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
-        <a class="navbar-brand" href="#">Quotinator</a>
+        <a class="navbar-brand" href="/">Quotinator</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
